3e5974a1cecd4d972617189e09c8addb9fe2a073
qlinnwu <jacqueline.wu18@gmail.com>
2025-08-08 20:34:23 -0400

    Initial auth setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider.tsx";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <App />
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>
