5b7a60c019b932649ddf5233193f6d421a8804ed
qlinnwu <jacqueline.wu18@gmail.com>
2025-08-13 00:42:53 -0400

    Create album page

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -7,8 +7,10 @@ interface MusicStore {
   albums: Album[];
   isLoading: boolean;
   error: string | null;
+  currentAlbum: Album | null;
 
   fetchAlbums: () => Promise<void>;
+  fetchAlbumById: (id: string) => Promise<void>;
 }
 
 export const useMusicStore = create<MusicStore>((set) => ({
@@ -16,10 +18,10 @@ export const useMusicStore = create<MusicStore>((set) => ({
   songs: [],
   isLoading: false,
   error: null,
+  currentAlbum: null,
 
   fetchAlbums: async () => {
     set({isLoading: true, error: null});
-
     try {
       const response = await axiosInstance.get("/albums");
       set({albums: response.data});
@@ -27,6 +29,18 @@ export const useMusicStore = create<MusicStore>((set) => ({
       set({error: error.response.data.message});
     } finally {
       set({ isLoading: false });
+    }
+  },
+
+  fetchAlbumById: async (id) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get(`/albums/${id}`);
+      set({ currentAlbum: response.data });
+    } catch (error: any) {
+      set({ error: error.response.data.message });
+    } finally {
+      set({ isLoading: false })
     }
   },
 }));
